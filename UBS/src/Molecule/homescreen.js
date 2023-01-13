4c045dbb8da4d02bf79100b52a5f012623d512ca
Song Yu Xiang <yxsong.2020@scis.smu.edu.sg>
2023-01-13 14:30:13 +0800

    SSO Photo, HomeScreen JS

--- UBS/src/Molecule/homescreen.js
+++ UBS/src/Molecule/homescreen.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import { SafeAreaView,ScrollView,StatusBar,StyleSheet,Text,useColorScheme,View,Section, Pressable} from 'react-native';
+import { SafeAreaView,ScrollView,StatusBar,StyleSheet,Text,useColorScheme,View,Section, Pressable, Image} from 'react-native';
 
+const styles = StyleSheet.create({
+    background : {
+        width: "100%",
+        height: "100%"
+    }
+})
 
 export default function HomeScreen() {
   return (
-    <></>
+    <View>
+        <Image source={require("../../media/Environment/SSO.png")} style={styles.background} />
+    </View>
   )
 }
