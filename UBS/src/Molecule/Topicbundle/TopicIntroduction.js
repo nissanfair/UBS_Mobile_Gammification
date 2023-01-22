325e4d202f5d25df85223e140f77ff196db2fc5b
Wong Jie Peng <76239879+jiepengwong@users.noreply.github.com>
2023-01-22 20:57:11 +0800

    links and api calls for intro page setup

--- UBS/src/Molecule/Topicbundle/TopicIntroduction.js
+++ UBS/src/Molecule/Topicbundle/TopicIntroduction.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, Dimensions, Image, ScrollView} from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -10,11 +10,28 @@ const TopicIntroduction = () => {
     const navigation = useNavigation();
     // Get relevant information from the store
 
+    // Get what topic to be displayed here
     const topic = useSelector(state => state.topic.topic);
+    
     const [topicChosen, setTopic] = useState(topic);
+    const [introSelectedTopic, pushIntro] = useState("");
 
     useEffect(() => {
         // Idea
+        var fetchSelectedTopic =   `http://10.0.2.2:3000/6bit/topics/${topic}`;
+
+        fetch(fetchSelectedTopic)
+        .then(response => response.json())
+        .then(data => {
+            // pushing the intro here. We can add more stuff here then use redux accordingly 
+            pushIntro(data.data[`${topic}_Introduction`])
+
+        })
+        .catch(error => {
+            console.log(error)
+        })
+
+
         
     
     
@@ -26,7 +43,7 @@ const TopicIntroduction = () => {
 
     return (
         <Text>
-            {topicChosen}
+            {introSelectedTopic}
             This is the topic introduction page; 
         </Text>
     );
