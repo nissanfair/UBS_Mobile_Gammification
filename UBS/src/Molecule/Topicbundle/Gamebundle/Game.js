8636d3da343d39cda3dc0e260de7413278a17580
Song Yu Xiang <yxsong.2020@scis.smu.edu.sg>
2023-02-03 22:23:02 +0800

    Fixed Game Js, there is still problem with the Image Background

--- UBS/src/Molecule/Topicbundle/Gamebundle/Game.js
+++ UBS/src/Molecule/Topicbundle/Gamebundle/Game.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { StyleSheet, View, Text, Dimensions, Image, ScrollView, TouchableHighlight, ImageBackground} from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Question from './Questions';
+import Health from './Health'
 
 const Stack = createStackNavigator();
 
@@ -21,12 +23,11 @@ const Game = () => {
 
     return (
         <View>
-            <TouchableHighlight >
-                <Image source={require("../../../../media/Environment/craftpix-897715-free-pixel-art-fantasy-2d-battlegrounds/PNG/Battleground1/Pale/Battleground1.png")} style={styles.background} />
+                {/* <ImageBackground source={require("../../../../media/Environment/craftpix-897715-free-pixel-art-fantasy-2d-battlegrounds/PNG/Battleground1/Pale/Battleground1.png")} style={styles.background}/> */}
                 {/* Enter your components here */}
-                
+                    <Question/>
+                    <Health/>
                 {/* End of Components here */}
-            </TouchableHighlight>
         </View>
     );
 };
