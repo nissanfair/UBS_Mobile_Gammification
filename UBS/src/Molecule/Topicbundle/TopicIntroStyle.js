975f3a25aab0ff725805980b6c8bb95cab1eea0a
Marcus Leo <marcleo97@hotmail.com>
2023-02-08 12:18:41 +0800

    Topic Introduction back & start buttons
    Added the final back and start buttons CSS to the topic introduction screen

--- UBS/src/Molecule/Topicbundle/TopicIntroStyle.js
+++ UBS/src/Molecule/Topicbundle/TopicIntroStyle.js
@@ -1,55 +1,74 @@
 /* eslint-disable prettier/prettier */
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
+  main: {
+    width: '100%',
+    height: '100%',
+    flexDirection: 'row',
+  },
 
-    main: {
-        width: '100%',
-        height: '100%',
-        flexDirection: 'row'
-    },
+  leftbox: {
+    width: '100%',
+    height: '100%',
+    flex: 4,
+  },
+  rightbox: {
+    width: '100%',
+    height: '100%',
+    flex: 3,
+  },
+  innerrightbox:{
+    flex: 1,
+    flexDirection: 'column',
+  },
+  monsterbox: {
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonbox: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  backgroundContainer: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  backdrop: {
+    width: '100%',
+    height: '100%',
+  },
+  leftbackdrop: {
+    width: '100%',
+    height: '100%',
+  },
 
-    leftbox: {
-        width: '100%',
-        height: '100%',
-        flex: 3,
-    },
-    rightbox: {
-        width: '100%',
-        height: '100%',
-        flex:3,
-        
-    },
-    monsterbox:{
-        width: '100%',
-        height: '80%',
-    },
-    backgroundContainer: {
-        position: 'absolute',
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-    },
-    backdrop: {
-        width: '100%',
-        height: '100%',
-    },
-    leftbackdrop: {
-        width: '100%',
-        height: '100%',
-    },
-
-    desc: {
-        position: 'absolute',
-        top: 0,
-        left: 35,
-        right: 35,
-        bottom: 0,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+  desc: {
+    position: 'absolute',
+    top: '0%',
+    left: '10%',
+    right: '10%',
+    bottom: '0%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  wizard: {
+    height: '100%',
+    width: '100%',
+  },
+  start:{
+    flex:3,
+    flexDirection: 'row', 
+    justifyContent: 'flex-end'
+  },
+  back:{
+    flex:3,
+  }
 
 });
 
-export { styles }
+export {styles};
--- UBS/src/Molecule/Topicbundle/TopicIntroduction.js
+++ UBS/src/Molecule/Topicbundle/TopicIntroduction.js
@@ -1,94 +1,100 @@
 /* eslint-disable prettier/prettier */
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Dimensions, Image, ScrollView, ImageBackground } from 'react-native';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { useSelector } from 'react-redux';
-import { styles } from "./TopicIntroStyle";
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  Image,
+  ScrollView,
+  ImageBackground,
+} from 'react-native';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {useSelector} from 'react-redux';
+import {styles} from './TopicIntroStyle';
 
 const Stack = createStackNavigator();
 
 const TopicIntroduction = () => {
-    const navigation = useNavigation();
-    // Get relevant information from the store
+  const navigation = useNavigation();
+  // Get relevant information from the store
 
-    // Get what topic to be displayed here
-    // Redux
-    // useSelector()
-    // dispatch()
-    const topic = useSelector(state => state.topic.topic);
+  // Get what topic to be displayed here
+  // Redux
+  // useSelector()
+  // dispatch()
+  const topic = useSelector(state => state.topic.topic);
 
-    const [topicChosen, setTopic] = useState(topic);
-    const [introSelectedTopic, pushIntro] = useState("");
+  const [topicChosen, setTopic] = useState(topic);
+  const [introSelectedTopic, pushIntro] = useState('');
 
+  useEffect(() => {
+    // Idea
+    var fetchSelectedTopic = `http://10.0.2.2:3000/6bit/topics/${topic}`;
 
+    fetch(fetchSelectedTopic)
+      .then(response => response.json())
+      .then(data => {
+        // pushing the intro here. We can add more stuff here then use redux accordingly
+        pushIntro(data.data[`${topic}_Introduction`]);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
-    useEffect(() => {
-        // Idea
-        var fetchSelectedTopic = `http://10.0.2.2:3000/6bit/topics/${topic}`;
+  return (
+    <View style={styles.main}>
+      <View style={styles.backgroundContainer}>
+        <Image
+          source={require('../../../media/Environment/s4m_ur4i-bg_clouds.png')}
+          resizeImage="stretch"
+          style={styles.backdrop}
+        />
+      </View>
 
-        fetch(fetchSelectedTopic)
-            .then(response => response.json())
-            .then(data => {
-                // pushing the intro here. We can add more stuff here then use redux accordingly 
-                pushIntro(data.data[`${topic}_Introduction`])
+      <View style={styles.leftbox}>
+        <ImageBackground
+          source={require('../../../media/Environment/panel_Example2.png')}
+          resizeMode="stretch"
+          style={styles.leftbackdrop}
+        />
 
-            })
-            .catch(error => {
-                console.log(error)
-            })
-
-
-
-
-
-
-    }, [])
-
-
-
-
-
-
-
-    return (
-        <View style={styles.main}>
-
-            <View style={styles.backgroundContainer}>
-                <Image source={require("../../../media/Environment/s4m_ur4i-bg_clouds.png")} resizeImage='stretch' style={styles.backdrop} />
-
-                
-            </View>
-
-
-            <View style={styles.leftbox}>
-                <ImageBackground source={require("../../../media/Environment/panel_Example2.png")} resizeMode='stretch' style={styles.leftbackdrop} />
-
-                <View style={styles.desc}>
-                    <Text>
-                        {introSelectedTopic}
-                    </Text>
-                </View>
-
-
-            </View>
-
-            <View style={styles.rightbox}>
-                
-                <View style={styles.monsterbox}>
-                <ImageBackground source={require("../../../media/Characters/EVil Wizard 2/Sprites/Idle.png")} resizeMode='stretch' style={styles.leftbackdrop} />
-
-                </View>
-
-                <View>
-                    <Text>Buttons to be placed here</Text>
-                </View>
-
-            </View>
-
-
+        <View style={styles.desc}>
+          <Text>{introSelectedTopic}</Text>
         </View>
-    );
+      </View>
+
+      <View style={styles.rightbox}>
+        <View style={styles.innerrightbox}>
+          <View style={styles.monsterbox}>
+            <Image
+              source={require('../../../media/Characters/sorcerer_villain/villian_clear.png')}
+              resizeMode="contain"
+              styles={styles.wizard}
+            />
+            <Text>Malware Wizard</Text>
+          </View>
+
+          <View style={styles.buttonbox}>
+            <View style={styles.back}>
+              <Image
+                source={require('../../../media/UI/back_v2.png')}
+                resizeMode="contain"
+              />
+            </View>
+            <View style={styles.start}>
+              <Image
+                source={require('../../../media/UI/start_v2.png')}
+                resizeMode="contain"
+              />
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 };
 
-export default TopicIntroduction
+export default TopicIntroduction;
