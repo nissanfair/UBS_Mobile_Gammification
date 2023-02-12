f6c27b60790cbd61ec0bc74a146dac5feb7913b1
LIM-YU-XUAN <76712654+LIM-YU-XUAN@users.noreply.github.com>
2023-02-12 08:45:31 +0800

    learning page v2

--- UBS/src/Molecule/Topicbundle/TopicLearning.js
+++ UBS/src/Molecule/Topicbundle/TopicLearning.js
@@ -1,75 +1,81 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Dimensions, Image, ScrollView, ImageBackground } from 'react-native';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { useSelector } from 'react-redux';
-import { styles } from "./TopicIntroStyle";
+/* eslint-disable prettier/prettier */
+// eslint-disable prettier/prettier
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View, Text, Dimensions, Image, ScrollView, ImageBackground} from 'react-native';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {useSelector} from 'react-redux';
+import {styles} from './TopicIntroStyle';
+
 const Stack = createStackNavigator();
 
 const TopicLearning = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const topic = useSelector(state => state.topic.topic);
+  const topic = useSelector(state => state.topic.topic);
 
-    const [topicChosen, setTopic] = useState(topic);
-    const [introSelectedTopic, pushIntro] = useState("");
+  const [topicChosen, setTopic] = useState(topic);
+  const [learningSelectedTopic, pushLearn] = useState('');
 
-    useEffect(() => {
-        // Idea
-        var fetchSelectedTopic = `http://10.0.2.2:3000/6bit/topics/${topic}`;
+  useEffect(() => {
+    var fetchSelectedTopic = `http://10.0.2.2:3000/6bit/topics/${topic}`;
 
-        fetch(fetchSelectedTopic)
-            .then(response => response.json())
-            .then(data => {
-                pushIntro(data.data[`${topic}_Learning`])
+    fetch(fetchSelectedTopic)
+      .then(response => response.json())
+      .then(data => {
+        pushLearn(data.data['${topic}_Learning']);
+      })
 
-            })
-            .catch(error => {
-                console.log(error)
-            })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
-    },[])
+  return (
+    <View style={styles.main}>
 
-    return (
-        <View style={styles.main}>
+      <View style={styles.backgroundContainer}>
+        <Image
+          source={require('../../../media/Environment/learning_background.png')}
+          resizeImage="stretch"
+          style={styles.backdrop}/>
+      </View>
 
-        <View style={styles.backgroundContainer}>
-            <Image source={require("../../../media/Environment/learning_background.png")} resizeImage='stretch' style={styles.backdrop} />
+      <Text>Welcome fighter! Here, you will learn more about this topic!</Text>
+
+      <View style={styles.leftbox}>
+        {/* <ImageBackground source={require("../../../media/Environment/textboxImage.png")} 
+                resizeMode='stretch' 
+                style={styles.leftbackdrop} /> */}
+      </View>
+
+        <View style={styles.desc}>
+          <Text> {learningSelectedTopic} 
+          TBC0 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </Text>
         </View>
 
-        <Text>
-            Welcome fighter! Here, you will learn more about this topic!
-        </Text>
-
-
-        <View style={styles.leftbox}>
-            <ImageBackground source={require("../../../media/Environment/textboxImage.png")} resizeMode='stretch' style={styles.leftbackdrop} />
-
-            <View style={styles.desc}>
-                <Text>
-                    {introSelectedTopic}
-                </Text>
-            </View>
-
-
-        </View>
+          <Image
+            source={require('../../../media/UI/skipButton.png')}
+            resizeMode="contain"/>
+        <Text> Skip All Learning</Text>
 
         <View style={styles.rightbox}>
-
-            <Text>
-                button 1. click next (is483 - 21) hahahaha
-            </Text>
-        <Text>
-           button 2. click skip (is483 - 24)
-        </Text>
-
-
+            <View style={styles.innerrightbox}>
+                <View style={styles.monsterbox}></View>
+                    <View style={styles.buttonbox}>
+                        <View style={styles.back}></View>
+                            <View style={styles.start}>
+                            <Image
+                                source={require('../../../media/UI/nextButton.png')}
+                                resizeMode="contain"/>
+            </View>
+          </View>
         </View>
-
+      </View>
 
     </View>
-       
-    );
+  );
 };
 
-export default TopicLearning
+export default TopicLearning;
