ee70efd1a61f8814f42eb55b614e72ad87fc1f51
LIM-YU-XUAN <76712654+LIM-YU-XUAN@users.noreply.github.com>
2023-02-19 18:48:00 +0800

    topic learning v4 - fixed pages without indexing

--- UBS/src/Molecule/Topicbundle/TopicLearning.js
+++ UBS/src/Molecule/Topicbundle/TopicLearning.js
@@ -11,6 +11,8 @@ const Stack = createStackNavigator();
 const TopicLearning = () => {
   const navigation = useNavigation();
 
+const[Index, setIndex] = useState(0);
+
   const Backbutton = () => {
    navigation.navigate("TopicLearning2")
   };
@@ -22,6 +24,10 @@ const TopicLearning = () => {
   const Exitbutton = () => {
     navigation.navigate("Topic")
   };
+
+  const Increment = () => {
+    setIndex(Index + 1)
+  }
 
   const topic = useSelector(state => state.topic.topic);
 
@@ -43,55 +49,111 @@ const TopicLearning = () => {
   }, []);
 
   return (
+    // without indexing
     <View style={styles.main}>
 
-      <View style={styles.backgroundContainer}>
-        <Image
-          source={require('../../../media/Environment/topicLearningBackground.png')}
-          resizeImage="stretch"
-          style={styles.backdrop}/>
-      </View>
+    <View style={styles.backgroundContainer}>
+      <Image
+        source={require('../../../media/Environment/topicLearningBackground.png')}
+        resizeImage="stretch"
+        style={styles.backdrop}/>
+    </View>
 
-      <View style={styles.leftbox}>
-        <TouchableOpacity style={styles.button} onPress={Backbutton}>
+    <View style={styles.leftbox}>
+      <TouchableOpacity style={styles.button} onPress={Backbutton}>
+        <Image
+          source={require('../../../media/UI/previous.png')}
+          resizeMode="contain"/>
+      </TouchableOpacity>
+    </View>
+
+    <View style={styles.middlebox}>
+      <ImageBackground
+        source={require('../../../media/UI/topicLearningScroll.png')}
+        resizeMode="contain"
+        style={styles.leftbackdrop}/>
+
+      <View style={styles.desc}>
+        <Text style={{color: "black"}}>{learnSelectedTopic[0]}
+        </Text>
+
+      </View>
+    </View>
+
+    <View style={styles.rightbox}>
+      <View style>
+        <TouchableOpacity onPress={Forwardbutton}>
           <Image
-            source={require('../../../media/UI/previous.png')}
+            source={require('../../../media/UI/forward.png')}
             resizeMode="contain"/>
         </TouchableOpacity>
       </View>
 
-      <View style={styles.middlebox}>
-        <ImageBackground
-          source={require('../../../media/UI/topicLearningScroll.png')}
-          resizeMode="contain"
-          style={styles.leftbackdrop}/>
-
-        <View style={styles.desc}>
-          <Text style={{color: "black"}}>{learnSelectedTopic[0]}
-          </Text>
-
-        </View>
+      <View style={styles.exitbutton}>
+        <TouchableOpacity style={styles.exitbutton} onPress={Exitbutton}>
+          <Image
+            source={require('../../../media/UI/exit.png')}
+            resizeMode="contain"/>
+        </TouchableOpacity>
       </View>
-
-      <View style={styles.rightbox}>
-        <View style>
-          <TouchableOpacity onPress={Forwardbutton}>
-            <Image
-              source={require('../../../media/UI/forward.png')}
-              resizeMode="contain"/>
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.exitbutton}>
-          <TouchableOpacity style={styles.exitbutton} onPress={Exitbutton}>
-            <Image
-              source={require('../../../media/UI/exit.png')}
-              resizeMode="contain"/>
-          </TouchableOpacity>
-        </View>
-      </View>
-
     </View>
+
+  </View>
+    
+    // // console.log(Index),
+    // <View style={styles.main}>
+    // </View>,
+
+    //   {Index ?
+    //   <View style={styles.backgroundContainer}>
+    //     <Image
+    //       source={require('../../../media/Environment/topicLearningBackground.png')}
+    //       resizeImage="stretch"
+    //       style={styles.backdrop}/>
+    //   </View>
+
+    //   <View style={styles.leftbox}>
+    //     <TouchableOpacity style={styles.button} onPress={Backbutton}>
+    //       <Image
+    //         source={require('../../../media/UI/previous.png')}
+    //         resizeMode="contain"/>
+    //     </TouchableOpacity>
+    //   </View>
+
+    //   <View style={styles.middlebox}>
+    //     <ImageBackground
+    //       source={require('../../../media/UI/topicLearningScroll.png')}
+    //       resizeMode="contain"
+    //       style={styles.leftbackdrop}/>
+
+    //     <View style={styles.desc}>
+    //       <Text style={{color: "black"}}>{learnSelectedTopic[0]}
+    //       </Text>
+
+    //     </View>
+    //   </View>
+
+    //   <View style={styles.rightbox}>
+    //     <View style>
+    //       <TouchableOpacity onPress={Forwardbutton}>
+    //         <Image
+    //           source={require('../../../media/UI/forward.png')}
+    //           resizeMode="contain"/>
+    //       </TouchableOpacity>
+    //     </View>
+
+    //     <View style={styles.exitbutton}>
+    //       <TouchableOpacity style={styles.exitbutton} onPress={Exitbutton}>
+    //         <Image
+    //           source={require('../../../media/UI/exit.png')}
+    //           resizeMode="contain"/>
+    //       </TouchableOpacity>
+    //     </View>
+    //   </View>
+    //   :
+
+    //   }
+
   );
 };
 
--- UBS/src/Molecule/Topicbundle/TopicLearningTEMP.js
+++ UBS/src/Molecule/Topicbundle/TopicLearningTEMP.js
@@ -2,111 +2,98 @@
 
 // PLEASE READ: this file is temporary and will be removed.
 
-
 import React, {useState, useEffect} from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  Dimensions,
-  Image,
-  ScrollView,
-  ImageBackground,
-  TouchableOpacity,
-} from 'react-native';
+import {StyleSheet, View, Text, Dimensions, Image, ScrollView, ImageBackground, TouchableOpacity } from 'react-native';
 import {NavigationContainer, useNavigation} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {useSelector} from 'react-redux';
 import {styles} from './TopicLearnStyle';
+
 const Stack = createStackNavigator();
 
 const TopicLearning2 = () => {
   const navigation = useNavigation();
+
   const Backbutton = () => {
-   // navigation.navigate("Stages")
    navigation.navigate("TopicLearning")
-  }
+  };
+
   const Forwardbutton = () => {
-    //navigation.navigate("Topic")
-    navigation.navigate("TopicLearning")
-  }
+    navigation.navigate("TopicLearning2")
+  };
 
   const Exitbutton = () => {
     navigation.navigate("Topic")
-  }
+  };
 
   const topic = useSelector(state => state.topic.topic);
 
   const [topicChosen, setTopic] = useState(topic);
-  const [introSelectedTopic, pushIntro] = useState('');
+  const [learnSelectedTopic, pushLearn] = useState('');
 
   useEffect(() => {
-    // Idea
     var fetchSelectedTopic = `http://10.0.2.2:3000/6bit/topics/${topic}`;
+    console.log(topic);
 
     fetch(fetchSelectedTopic)
       .then(response => response.json())
       .then(data => {
-        pushIntro(data.data[`${topic}_Learning`]);
+        pushLearn(data.data[`${topic}_Learning`]);
       })
       .catch(error => {
         console.log(error);
       });
-  });
+  }, []);
 
   return (
     <View style={styles.main}>
+
       <View style={styles.backgroundContainer}>
         <Image
-          source={require('../../../media/Environment/learning_background.png')}
+          source={require('../../../media/Environment/topicLearningBackground.png')}
           resizeImage="stretch"
-          style={styles.backdrop}
-        />
+          style={styles.backdrop}/>
       </View>
 
       <View style={styles.leftbox}>
         <TouchableOpacity style={styles.button} onPress={Backbutton}>
           <Image
             source={require('../../../media/UI/previous.png')}
-            resizeMode="contain"
-          />
+            resizeMode="contain"/>
         </TouchableOpacity>
       </View>
 
       <View style={styles.middlebox}>
         <ImageBackground
-          source={require('../../../media/UI/learn_board.png')}
+          source={require('../../../media/UI/topicLearningScroll.png')}
           resizeMode="contain"
-          style={styles.leftbackdrop}
-        />
+          style={styles.leftbackdrop}/>
+
         <View style={styles.desc}>
-          <Text style={{color: "white"}}>{introSelectedTopic}
-            Malware Attacks, short for "malicious software", and it refers to any intrusive software developed by criminals to steal data and damage or destroy computers and computer systems!
-
-            What's more, According to AV-TEST Institute, 450,000 new malicious programs are registered every day!
-
+          <Text style={{color: "black"}}>{learnSelectedTopic[1]}
           </Text>
+
         </View>
       </View>
 
       <View style={styles.rightbox}>
-      <TouchableOpacity style={styles.button} onPress={Forwardbutton}>
-          <Image
-            source={require('../../../media/UI/forward.png')}
-            resizeMode="contain"
-          />
-        </TouchableOpacity>
+        <View style>
+          <TouchableOpacity onPress={Forwardbutton}>
+            <Image
+              source={require('../../../media/UI/forward.png')}
+              resizeMode="contain"/>
+          </TouchableOpacity>
+        </View>
 
         <View style={styles.exitbutton}>
           <TouchableOpacity style={styles.exitbutton} onPress={Exitbutton}>
             <Image
               source={require('../../../media/UI/exit.png')}
-              resizeMode="contain"
-            />
+              resizeMode="contain"/>
           </TouchableOpacity>
         </View>
-    
       </View>
+
     </View>
   );
 };
