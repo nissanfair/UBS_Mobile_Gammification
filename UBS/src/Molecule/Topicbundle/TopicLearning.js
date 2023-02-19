b09b336ce2bce226d4f1ebebc18c03f896e6d537
LIM-YU-XUAN <76712654+LIM-YU-XUAN@users.noreply.github.com>
2023-02-19 19:51:33 +0800

    topic learning v4 - topic learning completed

--- UBS/src/Molecule/Topicbundle/TopicLearning.js
+++ UBS/src/Molecule/Topicbundle/TopicLearning.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, View, Text, Dimensions, Image, ScrollView, ImageBackground, TouchableOpacity } from 'react-native';
+import {StyleSheet, View, Text, Dimensions, Image, ScrollView, ImageBackground, TouchableOpacity, TouchableHighlight} from 'react-native';
 import {NavigationContainer, useNavigation} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {useSelector} from 'react-redux';
@@ -27,6 +27,10 @@ const[Index, setIndex] = useState(0);
     navigation.navigate("Topic")
   };
 
+  const Startbutton = () => {
+    navigation.navigate("Game")
+  }
+
   // const Increment = () => {
   //   setIndex(Index + 1)
   // }
@@ -39,6 +43,7 @@ const[Index, setIndex] = useState(0);
   useEffect(() => {
     var fetchSelectedTopic = `http://10.0.2.2:3000/6bit/topics/${topic}`;
     console.log(topic);
+    console.log(Index);
 
     fetch(fetchSelectedTopic)
       .then(response => response.json())
@@ -107,47 +112,20 @@ const[Index, setIndex] = useState(0);
 
   if (Index === 0) {
     return (
+      // <View>
+      //     <TouchableHighlight onPress={setIndex(Index === 1)}>
+      //       <Image source={require('../../../media/Environment/topicLearning1.png')} />
+      //     </TouchableHighlight>
+      // </View>
       <View style={styles.main}>
 
         <View style={styles.backgroundContainer}>
-          <Image
-            source={require('../../../media/Environment/topicLearningBackground.png')}
-            resizeImage="stretch"
-            style={styles.backdrop}/>
-        </View>
-
-        <View style={styles.leftbox}>
-        </View>
-
-        <View style={styles.middlebox}>
-          <ImageBackground
-            source={require('../../../media/UI/topicLearningScroll.png')}
-            resizeMode="contain"
-            style={styles.leftbackdrop}/>
-
-          <View style={styles.desc}>
-            <Text style={{color: "black"}}>{learnSelectedTopic[0]}
-            </Text>
-
-          </View>
-        </View>
-
-        <View style={styles.rightbox}>
-          <View style>
-            <TouchableOpacity onPress={Forwardbutton}>
-              <Image
-                source={require('../../../media/UI/forward.png')}
-                resizeMode="contain"/>
-            </TouchableOpacity>
-          </View>
-
-          <View style={styles.exitbutton}>
-            <TouchableOpacity style={styles.exitbutton} onPress={Exitbutton}>
-              <Image
-                source={require('../../../media/UI/exit.png')}
-                resizeMode="contain"/>
-            </TouchableOpacity>
-          </View>
+          <TouchableOpacity onPress={Forwardbutton}>
+            <Image
+              source={require('../../../media/Environment/topicLearning1.png')}
+              resizeImage="stretch"
+              style={styles.backdrop}/>
+          </TouchableOpacity>
         </View>
 
       </View>
@@ -180,7 +158,7 @@ const[Index, setIndex] = useState(0);
             style={styles.leftbackdrop}/>
 
           <View style={styles.desc}>
-            <Text style={{color: "black"}}>{learnSelectedTopic[1]}
+            <Text style={{color: "black"}}>{learnSelectedTopic[0]}
             </Text>
 
           </View>
@@ -234,7 +212,7 @@ const[Index, setIndex] = useState(0);
             style={styles.leftbackdrop}/>
 
           <View style={styles.desc}>
-            <Text style={{color: "black"}}>{learnSelectedTopic[2]}
+            <Text style={{color: "black"}}>{learnSelectedTopic[1]}
             </Text>
 
           </View>
@@ -288,6 +266,60 @@ const[Index, setIndex] = useState(0);
             style={styles.leftbackdrop}/>
 
           <View style={styles.desc}>
+            <Text style={{color: "black"}}>{learnSelectedTopic[2]}
+            </Text>
+
+          </View>
+        </View>
+
+        <View style={styles.rightbox}>
+          <View style>
+            <TouchableOpacity onPress={Forwardbutton}>
+              <Image
+                source={require('../../../media/UI/forward.png')}
+                resizeMode="contain"/>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.exitbutton}>
+            <TouchableOpacity style={styles.exitbutton} onPress={Exitbutton}>
+              <Image
+                source={require('../../../media/UI/exit.png')}
+                resizeMode="contain"/>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+      </View>
+    );
+  };
+
+  if (Index === 4) {
+    return (
+      <View style={styles.main}>
+
+        <View style={styles.backgroundContainer}>
+          <Image
+            source={require('../../../media/Environment/topicLearningBackground.png')}
+            resizeImage="stretch"
+            style={styles.backdrop}/>
+        </View>
+
+        <View style={styles.leftbox}>
+          <TouchableOpacity style={styles.button} onPress={Backbutton}>
+            <Image
+              source={require('../../../media/UI/previous.png')}
+              resizeMode="contain"/>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.middlebox}>
+          <ImageBackground
+            source={require('../../../media/UI/topicLearningScroll.png')}
+            resizeMode="contain"
+            style={styles.leftbackdrop}/>
+
+          <View style={styles.desc}>
             <Text style={{color: "black"}}>{learnSelectedTopic[3]}
             </Text>
 
@@ -299,9 +331,9 @@ const[Index, setIndex] = useState(0);
           </View>
 
           <View style={styles.exitbutton}>
-            <TouchableOpacity style={styles.exitbutton} onPress={Exitbutton}>
+            <TouchableOpacity style={styles.exitbutton} onPress={Startbutton}>
               <Image
-                source={require('../../../media/UI/exit.png')}
+                source={require('../../../media/UI/start_v2.png')}
                 resizeMode="contain"/>
             </TouchableOpacity>
           </View>
