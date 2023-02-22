4eb3d48be0c8e8c4c2b77703c9ace57e9b5cc57c
Marcus Leo <marcleo97@hotmail.com>
2023-02-22 21:25:34 +0800

    New UI Topic Introduction
    Added codes and new resized gif for the new UI for topic introduction

--- UBS/src/Molecule/Topicbundle/TopicIntroStyle.js
+++ UBS/src/Molecule/Topicbundle/TopicIntroStyle.js
@@ -7,80 +7,104 @@ const styles = StyleSheet.create({
     height: '100%',
     flexDirection: 'row',
   },
-
   leftbox: {
     flex: 1,
-    
   },
   rightbox: {
     flex: 1,
-    
+    flexDirection: 'column-reverse',
   },
-  centerbox:{
+  centerbox: {
     flex: 4,
-    flexDirection: "column"
-
-  },
-  topbox:{
-    borderColor: "green",
-    borderWidth: 5,
-    flex: 3,
-  },
-  innerrightbox:{
-    flex: 1,
     flexDirection: 'column',
   },
-  monsterbox: {
-    flex: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
+  topbox: {
+    flex: 3,
+    flexDirection: 'row',
   },
-  buttonbox: {
-    flex: 1,
-    flexDirection: "row",
+  lefttopbox: {
+    flex: 3,
+  },
+  monsterbox: {
+    top: '10%',
+    bottom: '0%',
+    left: '35%',
+    height: '70%',
+    width: '65%',
+    justifyContent: 'center',
+  },
+  monster: {
+    alignSelf: 'center',
+  },
+  righttopbox: {
+    flex: 3,
+  },
+  reward: {
+    top: '30%',
+    bottom: '0%',
+    width: '75%',
+    height: '70%',
+    justifyContent: 'center',
+  },
+  bottombox: {
+    flex: 3,
   },
   backgroundContainer: {
     position: 'absolute',
-    width: "100%",
-    height: "100%",
+    width: '100%',
+    height: '100%',
     justifyContent: 'center',
   },
-  
   backdrop: {
     width: '100%',
     height: '100%',
   },
-  leftbackdrop: {
-    width: '100%',
-    height: '100%',
-  },
-
   desc: {
-    position: 'absolute',
     top: '0%',
-    left: '10%',
-    right: '10%',
-    bottom: '0%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontFamily: 'PressStart2P-Regular',
+    left: '18%',
+    right: '13%',
+    width: '70%',
   },
   wizard: {
-    height: '50%',
-    width: '50%',
+    flex: 1,
   },
-  start:{
-    flex:3,
-    flexDirection: 'row', 
-    justifyContent: 'flex-end'
-  },
-  back:{
-    flex:3,
-    flexDirection: 'row', 
-    justifyContent: 'flex-start'
-  },
+  start: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
   
-
+  },
+  back: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  textstyle: {
+    color: 'white',
+    fontFamily: 'PressStart2P-Regular',
+    fontSize: 12,
+  },
+  textdescheaderstyle: {
+    color: 'red',
+    fontFamily: 'PressStart2P-Regular',
+    fontSize: 12,
+  },
+  textrewardheaderstyle: {
+    color: 'white',
+    fontFamily: 'PressStart2P-Regular',
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  textrewardstyle: {
+    color: 'gold',
+    fontFamily: 'PressStart2P-Regular',
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  textdescstyle: {
+    color: 'white',
+    fontFamily: 'PressStart2P-Regular',
+    fontSize: 10,
+    lineHeight: 18,
+  },
 });
 
 export {styles};
--- UBS/src/Molecule/Topicbundle/TopicIntroduction.js
+++ UBS/src/Molecule/Topicbundle/TopicIntroduction.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   StyleSheet,
   View,
@@ -10,21 +10,20 @@ import {
   ImageBackground,
   TouchableOpacity,
 } from 'react-native';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { useSelector } from 'react-redux';
-import { styles } from './TopicIntroStyle';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {useSelector} from 'react-redux';
+import {styles} from './TopicIntroStyle';
 
 const Stack = createStackNavigator();
 
-
-const TopicIntroduction = ({ navigation }) => {
+const TopicIntroduction = ({navigation}) => {
   const Backbutton = () => {
-    navigation.navigate("Topic")
-  }
+    navigation.navigate('Topic');
+  };
   const Startbutton = () => {
-    navigation.navigate("Game")
-  }
+    navigation.navigate('Game');
+  };
   // Get relevant information from the store
 
   // Get what topic to be displayed here
@@ -39,22 +38,22 @@ const TopicIntroduction = ({ navigation }) => {
   useEffect(() => {
     // Idea
     var fetchSelectedTopic = `http://10.0.2.2:3000/6bit/topics/${topic}`;
-    console.log(topic)
+    console.log(topic);
     fetch(fetchSelectedTopic)
       .then(response => response.json())
       .then(data => {
         // pushing the intro here. We can add more stuff here then use redux accordingly
-        pushIntro(data.data[`${topic}_Introduction`])
+        pushIntro(data.data[`${topic}_Introduction`]);
       })
       .catch(error => {
         console.log(error);
       });
-  },);
+  });
 
   return (
     <View style={styles.main}>
       <View style={styles.backgroundContainer}>
-        <Image
+        <ImageBackground
           source={require('../../../media/UI/matrix_bg.jpg')}
           resizeImage="stretch"
           style={styles.backdrop}
@@ -69,40 +68,54 @@ const TopicIntroduction = ({ navigation }) => {
               resizeMode="contain"
             />
           </TouchableOpacity>
-          <View style={styles.desc}>
-            <Text style={{ color: 'black', fontFamily: 'PressStart2P-Regular' }}>{introSelectedTopic}</Text>
-          </View>
         </View>
-
-
       </View>
 
       <View style={styles.centerbox}>
-      
         <ImageBackground
           source={require('../../../media/UI/computer_screen.png')}
           resizeImage="contain"
-          style={styles.backdrop}
-        />
-       
+          style={styles.backdrop}>
 
+          <View style={styles.topbox}>
+
+            <View style={styles.lefttopbox}>
+              <View style={styles.monsterbox}>
+                <Image
+                  source={require('../../../media/sorcereridle_sm.gif')}
+                  resizeMode="contain"
+                  style={styles.monster}
+                />
+                <Text style={styles.textstyle}>Malware Wizard</Text>
+              </View>
+
+            </View>
+
+            <View style={styles.righttopbox}>
+              <View style={styles.reward}>
+                <Text style={styles.textrewardheaderstyle}>Reward</Text>
+                <Text style={styles.textrewardstyle}>E$15000</Text>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.bottombox}>
+            <View style={styles.desc}>
+              <Text
+                style={styles.textdescheaderstyle}>
+                Description
+              </Text>
+              <Text
+                style={styles.textdescstyle}>
+                {introSelectedTopic}
+              </Text>
+            </View>
+          </View>
+        </ImageBackground>
       </View>
 
       <View style={styles.rightbox}>
-        <View style={styles.innerrightbox}>
-          <View style={styles.monsterbox}>
-            <Image
-              source={require('../../../media/sorcereridle.gif')}
-              resizeMode="contain"
-              styles={styles.wizard}
-            />
-            <Text style={{ color: 'white', fontFamily: 'PressStart2P-Regular' }}>Malware Wizard</Text>
-          </View>
-
-          <View style={styles.buttonbox}>
-
-            <View style={styles.start}>
-
+        <View style={styles.start}>
               <TouchableOpacity style={styles.button} onPress={Startbutton}>
                 <Image
                   source={require('../../../media/UI/start_v2.png')}
@@ -110,8 +123,6 @@ const TopicIntroduction = ({ navigation }) => {
                 />
               </TouchableOpacity>
             </View>
-          </View>
-        </View>
       </View>
     </View>
   );
