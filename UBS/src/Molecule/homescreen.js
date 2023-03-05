01c72d985344f9a8740993f530b97614592fe703
Zuhaili Amin <mohammadza.2020@scis.smu.edu.sg>
2023-03-06 02:27:24 +0800

    MANAGED TO SOLVE AUTOPLAY AND PAUSEONEXIT

--- UBS/src/Molecule/homescreen.js
+++ UBS/src/Molecule/homescreen.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { SafeAreaView,ScrollView,StatusBar,StyleSheet,Text,useColorScheme,View,Section, Pressable, Image, TouchableHighlight} from 'react-native';
 import {styles} from '../Styling';
 import adventure from '../../media/Soundtracks/main/adventure.wav';
 import Sound from 'react-native-sound';
+import { AppState } from 'react-native';
 
 // const styles = StyleSheet.create({
 //     background : {
@@ -10,25 +11,67 @@ import Sound from 'react-native-sound';
 //         height: "100%"
 //     }
 // })
+
 Sound.setCategory('Playback');
 
-export var adven = new Sound(adventure, (error) => {
-  if (error) {
-    console.log('failed to load the sound', error);
-    return;
-  }
-});
+// export var adven = new Sound(adventure, (error) => {
+//   if (error) {
+//     console.log('failed to load the sound', error);
+//     return;
+//   }
+// });
 
-
-
+var adven;
 
 export default function HomeScreen({navigation}) {
+  const [appState, setAppState] = useState(AppState.currentState);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    adven = new Sound(adventure, (error) => {
+      if (error) {
+        console.log('Error loading sound: ', error);
+      } else {
+        console.log('Sound loaded successfully');
+        setIsLoaded(true);
+        adven.setVolume(0.5);
+        adven.play();
+        adven.setNumberOfLoops(-1);
+      };
+    }
+    );
+  }, []);
+
+  useEffect(() => {
+    AppState.addEventListener('change', handleAppStateChange);
+    return () => {
+      AppState.removeEventListener('change', handleAppStateChange);
+    };
+  }, []);
+
+  const handleAppStateChange = (nextAppState) => {
+    if (nextAppState === 'active') {
+      // App has come to the foreground
+      // Start playing sound again
+        adven.setVolume(0.5);
+        adven.play();
+        adven.setNumberOfLoops(-1);
+    } else if (appState === 'active' && nextAppState.match(/inactive|background/)) {
+      // App has gone to the background
+      // Stop playing sound
+      adven.pause();
+    }
+    setAppState(nextAppState);
+  };
+  
   const GoogleSSO = () => {
     navigation.navigate("Topic")
-    adven.setVolume(0.5);
-    adven.play();
-    adven.setNumberOfLoops(-1);
+    // adven.setVolume(0.5);
+    // adven.play();
+    // adven.setNumberOfLoops(-1);
   }
+
+  
 
   return (
     <View>
@@ -38,3 +81,6 @@ export default function HomeScreen({navigation}) {
     </View>
   )
 }
+
+
+export { adven };
